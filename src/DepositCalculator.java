05ce782fb1f6fa588dbe65fe9c55d5ba3ec1e071
Truelove <pavel.koryakin1990@yandex.ru>
2022-06-06 22:56:58 +0300

    Code style check

---
+++ src/DepositCalculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+
+    int places = 2;
+    double yearRate = 0.06;
+
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod ) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return round(pay, places);
+    }
+
+    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
+        return round(amount + amount * yearRate * depositPeriod, places);
+    }
+
+    double round(double value, int places) {
+        double scaLe = Math.pow(10, places);
+        return Math.round(value * scaLe) / scaLe;
+    }
+
+    void depositCalculator() {
+        int period;
+        int action;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+        double outDoubleVar = 0;
+        if (action == 1) {
+            outDoubleVar = calculateSimplePercent(amount, yearRate, period);
+        } else if (action == 2) {
+            outDoubleVar = calculateComplexPercent(amount, yearRate, period);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().depositCalculator();
+    }
+
+
+
+
+}
